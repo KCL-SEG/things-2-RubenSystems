1b89f43f041252fd341758e4349c26b12bf2ee80
RubenSystems <ruben.ticehurst-james@kcl.ac.uk>
2022-10-06 14:25:22 +0100

    Added form.
    - Added it to the html file "home.html"
    - Created a form data type

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,13 @@
+from .models import Thing
+from django import forms
+
 """Forms of the project."""
 
 # Create your forms here.
+class ThingForm(forms.ModelForm): 
+
+	class Meta: 
+		model = Thing
+		fields = ['name', 'description', 'quantity']
+
+		widgets = {"description": forms.Textarea()}
